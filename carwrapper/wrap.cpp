a3c6cc4cf46ab2fe135bd24716904d34231362e9
nimrodouko <oukonimrod@gmail.com>
2025-01-10 20:12:11 +0000

    progress on the main file

--- carwrapper/wrap.cpp
+++ carwrapper/wrap.cpp
@@ -1,22 +1,39 @@
-
+#include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 void change_bordersize(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+const char* vertexshadersource = "#version 330 core\n"
+"layout (location =0) in vec3 apos; \n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(apos.x, apos.y, apos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentshadersource = "#version 330 core\n"
+"out vec4 fragcolor\n"
+"void main()\n"
+"{\n"
+"fragcolor = vec4(1.0f,0.5f,0.2f, 1.0f)\n"
+"}\0";
+
 int main(void)
 
 {
-    GLFWwindow* window;
 
     /* Initialize the library */
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR ,3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
     if (!glfwInit())
         return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    GLFWwindow* window;
+
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(800, 600, "car wrapper project", NULL, NULL);
@@ -29,20 +46,68 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+    {
+        std::cout<<"failed to initialize glad"<<std::endl;
+        return -1;
+    };
+    float vertices[] = {
+        -0.5f,-0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.5f,0.5f,0.0f
+    };
+    unsigned int VBO,VAO;
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+   
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int vertexshader;
+    vertexshader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexshader, 1, &vertexshadersource, NULL);
+    glCompileShader(vertexshader);
+
+    unsigned int fragmentshader;
+    fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentshader, 1, &fragmentshadersource, NULL);
+    glCompileShader(fragmentshader);
+
+    unsigned int shaderprogram;
+    shaderprogram = glCreateProgram();
+    glAttachShader(shaderprogram, vertexshader);
+    glAttachShader(shaderprogram, fragmentshader);
+    glLinkProgram(shaderprogram);
+
     while (!glfwWindowShouldClose(window))
     {
+
+        glUseProgram(shaderprogram);
+        glBindVertexArray(VAO);
+
         /* Render here */
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         processInput(window);
         glfwPollEvents();
+
+
     }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderprogram);
 
     glfwTerminate();
     return 0;
