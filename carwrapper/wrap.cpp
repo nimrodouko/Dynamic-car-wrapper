4691633c59eb2e7dd5356a83d5735c4b0ef69654
nimrodouko <oukonimrod@gmail.com>
2025-01-15 19:57:52 +0000

    camera operations

--- carwrapper/wrap.cpp
+++ carwrapper/wrap.cpp
@@ -9,7 +9,12 @@
 void change_bordersize(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+glm::vec3 cameraposition = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 camerafront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraup = glm::vec3(0.0f, 1.0f, 0.0f);
 
+float deltatime = 0.0f;
+float lastframe = 0.0f;
 
 
 int main(void)
@@ -45,18 +50,77 @@ int main(void)
         std::cout<<"failed to initialize glad"<<std::endl;
         return -1;
     };
+   
+    glm::vec3 cameratarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameradirection = glm::normalize(cameraposition - cameratarget);
+    //right axis
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraright = glm::cross(up, cameradirection);
+
+
+
 
     Shader shader("./vertexshader.txt","./fragmentshader.txt");
 
 
 
-
     float vertices[] = {
-       
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,0.0f,
     };
+    glm::vec3 cubepositions[] = {
+            glm::vec3(0.0f,  0.0f,  0.0f),
+            glm::vec3(2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3(2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3(1.3f, -2.0f, -2.5f),
+            glm::vec3(1.5f,  2.0f, -2.5f),
+            glm::vec3(1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
+
     unsigned int VBO,VAO;
     glGenVertexArrays(1, &VAO);
     
@@ -82,18 +146,60 @@ int main(void)
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-  
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0, 3);
-        shader.use();
-        glm::mat4 transform = glm::mat4(1.0f);
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5, 0.0f));
-        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        float currentframe = glfwGetTime();
+        deltatime = currentframe - lastframe;
+        lastframe = currentframe;
 
-        unsigned int transformloc = glGetUniformLocation(shader.ID, "transform");
-        glUniformMatrix4fv(transformloc, 1, GL_FALSE, glm::value_ptr(transform));
+        glEnable(GL_DEPTH_TEST);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  
+   
+
+        
+        shader.use();
+      
+
+        glm::mat4 view;
+        view = glm::lookAt(cameraposition, cameraposition + camerafront, up);
+
+
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+       
+
+
+        unsigned int viewloc = glGetUniformLocation(shader.ID, "view");
+        glUniformMatrix4fv(viewloc, 1, GL_FALSE, glm::value_ptr(view));
+
+
+        unsigned int projectionloc = glGetUniformLocation(shader.ID, "projection");
+        glUniformMatrix4fv(projectionloc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(VAO);
+        
+for (unsigned int i = 0; i < 10;i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubepositions[i]);
+            float angle = 20.0f * i;
+            if (i % 3 == 0) {
+                angle = glfwGetTime() * 25.0f;
+            }
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+           
+            unsigned int modelloc = glGetUniformLocation(shader.ID, "model");
+            glUniformMatrix4fv(modelloc, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        }
+       
+
+        
+
+
+        
 
         
         
@@ -122,6 +228,17 @@ void change_bordersize(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 void processInput(GLFWwindow* window) {
+
+    const float cameraspeed = 5.5f * deltatime;
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraposition -= cameraspeed * camerafront;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraposition += cameraspeed * camerafront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraposition -= cameraspeed * glm::normalize(glm::cross(camerafront, cameraup));
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraposition += cameraspeed * glm::normalize(glm::cross(camerafront, cameraup));
+
 }
